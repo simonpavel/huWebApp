dccfdb635f384b03e45b1fb765a55f10da954abe
Pavlo <simon.pavel@pdffiller.team>
2026-01-15 08:49:35 +0100

    Update service worker cache version to v6 with auto-cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "huwords-v5";
+const CACHE = "huwords-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,7 +8,17 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
